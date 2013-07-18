scala/scala

SI-7409 Par-Test: A crash is not a DNC for neg tests
A compiler crash does not count as Does Not Compile for
purposes of a negative test. Changing the test kind from
"neg" to "dnc" is out of scope for this PR.

Even if the user asks to update the check file with the
crash result, we must prevent him from doing so. Any
further attempts to update the check file with a crash
will dispatch the Scala SWAT squad which will race to
his location and physically restrain the user. Only
Martin holds the code which will allow the squad to
stand down. So make sure his cell is on next time you
want to --update-check.

A neg test will stop trying to compile after the first
failed round, which is all that matters.

By popular request, a new test outcome is emitted when
the check file is updated. It is called "Updated". It
even has its own short status, the double-plus that is
reminiscent of diff output.

src/partest/scala/tools/partest/nest/NestUI.scala
@@ -62,11 +62,14 @@ object NestUI {
 
   def statusLine(state: TestState) = {
     import state._
-    val word = bold(
-      if (isSkipped) yellow("--")
-      else if (isOk) green("ok")
-      else red("!!")
-    )
+    import TestState._
+    val colorizer = state match {
+      case s: Skip     => yellow
+      case s: Updated  => cyan
+      case s if s.isOk => green
+      case _           => red
+    }
+    val word = bold(colorizer(state.shortStatus))
     f"$word $testNumber - $testIdent%-40s$reasonString"
   }
 

lrytz/scala-partest

src/partest/scala/tools/partest/nest/NestUI.scala
@@ -62,11 +62,14 @@ object NestUI {
 
   def statusLine(state: TestState) = {
     import state._
-    val word = bold(
-      if (isSkipped) yellow("--")
-      else if (isOk) green("ok")
-      else red("!!")
-    )
+    import TestState._
+    val colorizer = state match {
+      case _: Skip     => yellow
+      case _: Updated  => cyan
+      case s if s.isOk => green
+      case _           => red
+    }
+    val word = bold(colorizer(state.shortStatus))
     f"$word $testNumber - $testIdent%-40s$reasonString"
   }
 